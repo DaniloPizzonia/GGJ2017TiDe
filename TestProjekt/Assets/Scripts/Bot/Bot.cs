48e4c2231432196ace4305547687d0f8564f3f0b
DevSkill92 <hagemann-moritz@web.de>
2017-01-20 21:45:07 +0100

    added values to bot class

--- TestProjekt/Assets/Scripts/Bot/Bot.cs
+++ TestProjekt/Assets/Scripts/Bot/Bot.cs
@@ -2,11 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using UnityEngine;
+using UnityEngine.Events;
 
 namespace unsernamespace
 {
-	public class Bot
+	public class Bot : MonoBehaviour
 	{
+		private float health;
+		private float speed;
+
+		[SerializeField]
+		public UnityEvent onChangHalth = new UnityEvent();
+		public UnityEvent OnChgangeHalth { get { return onChangHalth; } }
+
+		[SerializeField]
+		public UnityEvent onChangSpeed = new UnityEvent();
+		public UnityEvent OnChangSpeed { get { return onChangSpeed; } }
+
+		public float Health
+		{
+			get
+			{
+				return health;
+			}
+			set
+			{
+				health = value;
+				onChangHalth.Invoke();
+			}
+		}
+
+		public float Speed
+		{
+			get
+			{
+				return speed;
+			}
+			set
+			{
+				speed = value;
+				onChangSpeed.Invoke();
+			}
+		}
 	}
 }
