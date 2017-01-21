4e7703e86fd5049b8afbae16fac040971715d5cb
DevSkill92 <hagemann-moritz@web.de>
2017-01-21 03:59:11 +0100

    Implementation for tower manager

--- TestProjekt/Assets/Scripts/Tower/TowerManager.cs
+++ TestProjekt/Assets/Scripts/Tower/TowerManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,6 +15,36 @@ namespace unsernamespace
 			get
 			{
 				return tower_collection.ToArray();
+			}
+		}
+
+		public Tower Create( GameObject prefab , Action<Tower> callback=null )
+		{
+			GameObject container = GameObject.Instantiate( prefab );
+			Tower tower = container.GetComponent<Tower>();
+			if ( null == tower )
+			{
+				throw new Exception( "wrong prefab" );
+			}
+			if ( null != callback )
+			{
+				callback( tower );
+			}
+			lock ( tower_collection )
+			{
+				tower_collection.Add( tower );
+			}
+			return tower;
+		}
+
+		public void Unregister( Tower tower )
+		{
+			lock ( tower_collection )
+			{
+				if ( tower_collection.Contains( tower ) )
+				{
+					tower_collection.Add( tower );
+				}
 			}
 		}
 	}
