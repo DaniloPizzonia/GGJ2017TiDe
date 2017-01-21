9067161dc97dbd55c00c3b1b9e6ee60668834e28
DevSkill92 <hagemann-moritz@web.de>
2017-01-21 09:45:05 +0100

    Localization class added

---
+++ TestProjekt/Assets/Scripts/Localization.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace unsernamespace
+{
+	public class Localization : RootComponent
+	{
+		private Dictionary<string , string> data = new Dictionary<string , string>()
+		{
+			{ "HighTide" , "Ebbe" },
+			{ "LowTide" , "Flut" }
+		};
+
+		public string this[ string key ]
+		{
+			get
+			{
+				return data[ key ];
+			}
+		}
+
+	}
+}
