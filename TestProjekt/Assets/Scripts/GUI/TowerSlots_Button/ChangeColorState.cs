1da1e9de91d3567dafebd591abc08c7ecdd90d21
Danilo Pizzonia <danilo.pizzonia@gmx.net>
2017-01-21 01:44:09 +0100

    cmt

--- TestProjekt/Assets/Scripts/GUI/TowerSlots_Button/ChangeColorState.cs
+++ TestProjekt/Assets/Scripts/GUI/TowerSlots_Button/ChangeColorState.cs
@@ -7,27 +7,12 @@ using UnityEngine.UI;
 namespace unsernamespace{
 	public class ChangeColorState : MonoBehaviour {
 
+		public GameObject Tower = new GameObject();
 	    public Button[] button = new Button[3];
 
-		public void ChangeColor(int i) {
-			//Root.I.Get<>
-	        switch(i) {
-	            case 0:
-	                button[0].enabled = false;
-	                Debug.Log("Tower 1");
-	                break;
-	            case 1:
-	                button[1].enabled = false;
-	                Debug.Log("Tower 2");
-	                break;
-	            case 2:
-	                button[2].enabled = false;
-	                Debug.Log("Tower 3");
-	                break;
-
-	            default:
-	                break;
-	        }
+		public void ChangeColor() {
+			//Root.I.Get<>(Tower);
+			Root.I.Get<TowerManager>(Tower);
 	    }
 	}
 }
