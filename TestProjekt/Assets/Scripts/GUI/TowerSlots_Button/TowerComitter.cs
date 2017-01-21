4ad57841bf1caca744bdbe1fa9e0a4ee338b83ef
Danilo Pizzonia <danilo.pizzonia@gmx.net>
2017-01-21 02:10:23 +0100

    Added new Class TwoerComitter <Template>

---
+++ TestProjekt/Assets/Scripts/GUI/TowerSlots_Button/TowerComitter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace unsernamespace{
+	public class TowerComitter : MonoBehaviour {
+		[SerializeField]
+		private GameObject Tower = new GameObject();
+
+
+		// use this method to pass the selected tower to the TowerManager
+		public void CommitTower(object bla) {
+			
+		//	Root.I.Get<TowerManager>(bla);
+			Debug.Log ("hat geklappt");
+		}
+	}
+}
