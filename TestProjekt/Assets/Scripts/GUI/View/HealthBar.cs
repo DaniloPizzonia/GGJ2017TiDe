1197641a1e5e8e5247769bbdae76b48aba4d7fe9
DevSkill92 <hagemann-moritz@web.de>
2017-01-22 08:44:21 +0100

    new health bar

---
+++ TestProjekt/Assets/Scripts/GUI/View/HealthBar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace unsernamespace
+{
+	public class HealthBar : MonoBehaviour
+	{
+		[SerializeField]
+		private Transform inner;
+		[SerializeField]
+		private Bot bot;
+
+		private void Update()
+		{
+			if (
+					inner != null
+				&&	bot != null
+			)
+			{
+				float progress = (float)bot.Health / bot.MaxHealth;
+				inner.transform.localScale = new Vector3( progress , 1 , 1 );
+			}
+
+			Camera camera = Camera.main;
+			transform.LookAt( transform.position + camera.transform.rotation * Vector3.forward ,
+				camera.transform.rotation * Vector3.up );
+		}
+	}
+}
