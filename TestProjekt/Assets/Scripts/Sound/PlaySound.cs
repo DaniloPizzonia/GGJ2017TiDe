fc589ecea6a8a2b09ed2f6db5eb42767f1eae1f7
DevSkill92 <hagemann-moritz@web.de>
2017-01-22 11:51:00 +0100

    sound interface added

---
+++ TestProjekt/Assets/Scripts/Sound/PlaySound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace unsernamespace
+{
+	public class PlaySound : MonoBehaviour
+	{
+		public AudioClip[] Clips;
+
+		public void Play()
+		{
+			if ( Clips != null && Clips.Length > 0 )
+			{
+				int index = (int)Random.Range( 0 , Clips.Length - 1 );
+				Sound.I.Play( Clips[ index ] );
+			}
+		}
+	}
+}
---
+++ TestProjekt/Assets/Scripts/Sound/Sound.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+namespace unsernamespace
+{
+	public class Sound : MonoBehaviour
+	{
+		private AudioSource source;
+		public static Sound I
+		{
+			get
+			{
+				return FindObjectOfType<Sound>();
+			}
+		}
+
+		public void Play( AudioClip clip )
+		{
+			if ( source == null )
+			{
+				AudioListener listener = MonoBehaviour.FindObjectOfType<AudioListener>();
+				if ( listener == null )
+				{
+					return;
+				}
+				source = listener.gameObject.GetComponent<AudioSource>();
+				if ( source == null )
+				{
+					return;
+				}
+			}
+			source.PlayOneShot( clip );
+		}
+	}
+}
