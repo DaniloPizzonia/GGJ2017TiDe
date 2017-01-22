aca64ce5d26e6da541cefac967fd24c899beae16
DevSkill92 <hagemann-moritz@web.de>
2017-01-22 05:57:26 +0100

    scene skip button implemented

---
+++ TestProjekt/Assets/Scripts/GUI/SkipGameMode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace unsernamespace
+{
+	public class SkipGameMode : MonoBehaviour
+	{
+		private Button button;
+
+		private bool active
+		{
+			get
+			{
+				return Root.I.Get<GameModeManager>().Current is LowTide;
+			}
+		}
+
+		private void Awake()
+		{
+			button = GetComponent<Button>();
+		}
+
+		private void Update()
+		{
+			if ( null != button )
+			{
+				button.interactable = active;
+			}
+		}
+
+		public void Click()
+		{
+			if ( active )
+			{
+				Root.I.Get<GameModeManager>().Switch();
+			}
+		}
+	}
+}
